7c58b2067acca6edafd1ddb4082aacd9c5c5a52f
yqy <568334855@qq.com>
2022-08-01 21:06:34 +0800

    初始化配置

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import "assets/css/base.css";
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  resolve:{
+    alias: {
+      'assets':'@/assets',
+      'common':'@/common',
+      'components':'@/components',
+      'network':'@/network',
+      'views':'@/views',
+    }
+  },
+  // vue-cli 默认 babel-loader 忽略 node_modules
+// 当某个依赖(如 vue-baberrage)存在 ES6 语法时，vue 官方给出的解决方案是 transpileDependencies
   transpileDependencies: true
 })
