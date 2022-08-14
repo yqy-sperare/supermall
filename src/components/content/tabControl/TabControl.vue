43ead953fe62b5c459a05620c3f78aec90b18d25
yqy <568334855@qq.com>
2022-08-14 15:00:27 +0800

    tab-control

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="tab-control">
+    <!-- 使用props传值来决定 -->
+    <div
+      v-for="(item, index) in titles"
+      :key="item"
+      class="tab-control-item"
+      :class="{ active: index === currentIndex }"
+      @click="itemClick(index)"
+    >
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      // 当前被选中
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  /* 吸顶灯效果时，导航栏颜色 */
+  background-color: #fff;
+}
+.tab-control-item {
+  height: 40px;
+  line-height: 40px;
+  flex: 1;
+}
+/* 给span添加一个内边距5px上下左右 */
+.tab-control-item span {
+  padding: 5px;
+}
+.active {
+  color: var(--color-high-text);
+}
+.active span {
+  border-bottom: 3px solid var(--color-tint);
+}
+</style>
